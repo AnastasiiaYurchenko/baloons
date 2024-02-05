d36a3377ab29f8bbb8e472a62d7f2529eb2b2de3
AnastasiiaYurchenko <nastya.brok@ukr.net>
2024-02-05 20:40:24 +0200

    add Review Component

---
+++ src/components/Review/Review.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ReviewItem, ReviewText, Avatar, Author, BottomWrapper } from './Review.styled';
+
+const Review = ({ review }) => {
+    const { text, author } = review;
+  return (
+    <ReviewItem>
+          <ReviewText>{text}</ReviewText> 
+          <BottomWrapper>
+            <Avatar/>
+            <Author>{author}</Author>
+          </BottomWrapper>
+    </ReviewItem>
+  )
+}
+
+export default Review
