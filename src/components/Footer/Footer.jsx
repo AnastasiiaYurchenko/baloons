3f72955aa018022c275a7ee955454839309b1d69
AnastasiiaYurchenko <nastya.brok@ukr.net>
2024-02-06 17:27:23 +0200

    finish Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import { FooterSection, Text } from './Footer.styled'
+import React from 'react';
+import { Content, Text } from './Footer.styled';
 import { Container } from 'components/Layout/Layout.styled';
 import logo from '../../images/logo.png';
 
 const Footer = () => {
   return (
-    <FooterSection>
-          <Container>
-              
-              <a href="/">
-            <img src={logo} alt="Logo" width='104px' />
-              </a>
-              <Text>confetti &copy; 2023</Text>
-          </Container>   
-    </FooterSection>
-  )
-}
+    <footer>
+      <Container>
+        <Content>
+          <a href="/">
+            <img src={logo} alt="Logo" width="104px" />
+          </a>
+          <Text>confetti &copy; 2023</Text>
+        </Content>
+      </Container>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/Footer/Footer.styled.js
+++ src/components/Footer/Footer.styled.js
@@ -1,25 +1,26 @@
 import styled from 'styled-components';
 
-export const FooterSection = styled.footer`
-    padding: 32px 0;
-    @media screen and (min-width: 768px){
-    padding: 25px 0 28px 0;
-    }
+export const Content = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-top: 1px solid ${p => p.theme.colors.accentColor};
+  padding: 32px 0;
+  @media screen and (min-width: 768px) {
+    padding: 24px 0 28px 0;
+  }
 
-@media screen and (min-width: 1280px){
-padding: 24px 0 36px 0;
-    }
-
-`
-
+  @media screen and (min-width: 1280px) {
+    padding: 24px 0 36px 0;
+  }
+`;
 
 export const Text = styled.p`
-    font-family: 'InterMedium';
-    font-weight: 500;
-    font-size: 16px;
-    line-height: 1;;
-    letter-spacing: -0.01em;
-    text-align: center;
-    color: ${p => p.theme.colors.mainTextColor};
-`
-
+  font-family: 'InterMedium';
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 1;
+  letter-spacing: -0.01em;
+  text-align: center;
+  color: ${p => p.theme.colors.mainTextColor};
+`;
