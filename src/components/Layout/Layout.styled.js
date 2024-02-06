0d9caa15f4f6b7288adbd547c058f650bf0c7a3b
AnastasiiaYurchenko <nastya.brok@ukr.net>
2024-02-07 01:13:57 +0200

    change max-width of container

--- src/components/Layout/Layout.styled.js
+++ src/components/Layout/Layout.styled.js
@@ -1,19 +1,18 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-position: relative;
-margin: 0 auto;
-padding: 0 20px;
-width: 100%;
-max-width: 480px;
+  margin: 0 auto;
+  padding: 0 20px;
+  width: 100%;
+  max-width: 480px;
 
-@media screen and (min-width: 768px){
+  @media screen and (min-width: 768px) {
     padding: 0 30px;
     max-width: 768px;
-}
+  }
 
-@media screen and (min-width: 1280px){
+  @media screen and (min-width: 1280px) {
     padding: 0 112px;
-    max-width: 1280px;
-}
-`
+    max-width: 1440px;
+  }
+`;
