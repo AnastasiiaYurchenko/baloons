614bc5ee37757757db5d29945b28e662096ad516
AnastasiiaYurchenko <nastya.brok@ukr.net>
2024-02-18 22:40:44 +0200

    fix item colors in about grid

--- src/components/AboutGrid/AboutGrid.jsx
+++ src/components/AboutGrid/AboutGrid.jsx
@@ -6,40 +6,8 @@ const AboutGrid = ({ about }) => {
   return (
     <List>
       {about.map(aboutItem => {
-        return (
-          <div key={aboutItem.id}>
-            <AboutItem aboutItem={aboutItem} />
-          </div>
-        );
+        return <AboutItem key={aboutItem.id} aboutItem={aboutItem} />;
       })}
-      {/* <Item>
-        <ItemTitle>Kreatywne podejście i dbałość o szczegóły</ItemTitle>
-        <ItemText>
-          Specjaliści zapewniają doradztwo i kreatywność przy wyborze dekoracji
-          balonowych, z dbałością o szczegóły
-        </ItemText>
-      </Item>
-      <Item>
-        <ItemTitle>Materiały przyjazne dla środowiska</ItemTitle>
-        <ItemText>
-          Wykorzystywane są materiały najwyższej jakości, w tym biodegradowalne
-          materiały balonowe, aby pozostawić jak najmniejszy ślad na środowisku.
-        </ItemText>
-      </Item>
-      <Item>
-        <ItemTitle>Szeroka gama kolorów do personalizacji</ItemTitle>
-        <ItemText>
-          Balony i dekoracje są wybierane z szerokiej gamy kolorów, co pozwala
-          spersonalizować wystrój na każdą okazję.
-        </ItemText>
-      </Item>
-      <Item>
-        <ItemTitle>Gwarancja dostawy i montażu</ItemTitle>
-        <ItemText>
-          Firma gwarantuje nie tylko wysokiej jakości dostawę zamówień, ale
-          także ich montaż, zapewniając klientom kompleksową obsługę.
-        </ItemText>
-      </Item> */}
     </List>
   );
 };
